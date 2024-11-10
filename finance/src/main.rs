0024143381fc9953a93ca9e8d6c9ff528e128ea1
rexhanh <rexhanh123@gmail.com>
2024-11-10 17:11:14 -0500

    working prototype

--- finance/src/main.rs
+++ finance/src/main.rs
@@ -1,33 +1,171 @@
-use std::io;
-
+use color_eyre::Result;
 use ratatui::{
-    crossterm::event::{self, KeyCode, KeyEventKind},
-    style::Stylize,
-    widgets::Paragraph,
+    buffer::Buffer,
+    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    layout::{Constraint, Layout, Rect},
+    style::{Color, Stylize},
+    symbols,
+    text::Line,
+    widgets::{
+        Block, Borders, List, ListItem, ListState, Padding, Paragraph, StatefulWidget, Widget, Wrap,
+    },
     DefaultTerminal,
 };
-
-fn main() -> io::Result<()> {
-    let mut terminal = ratatui::init();
-    terminal.clear()?;
-    let app_result = run(terminal);
-    ratatui::restore();
-    app_result
+struct App {
+    should_quit: bool,
+    stock_list: StockList,
 }
 
-fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
-    loop {
-        terminal.draw(|frame| {
-            let greeting = Paragraph::new("Hello Ratatui! (press 'q' to quit)")
-                .white()
-                .on_blue();
-            frame.render_widget(greeting, frame.area());
-        })?;
+struct Stock {
+    name: String,
+    price: f64,
+}
 
-        if let event::Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                return Ok(());
-            }
+struct StockList {
+    stocks: Vec<Stock>,
+    state: ListState,
+}
+
+impl Stock {
+    fn new(name: &str, price: f64) -> Self {
+        Self {
+            name: name.to_string(),
+            price,
         }
     }
+}
+
+impl StockList {
+    fn new() -> Self {
+        Self {
+            stocks: Vec::new(),
+            state: ListState::default(),
+        }
+    }
+
+    fn add_stock(&mut self, stock: Stock) {
+        self.stocks.push(stock);
+    }
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl App {
+    fn new() -> Self {
+        let mut stock_list = StockList::new();
+        stock_list.add_stock(Stock::new("AAPL", 129.41));
+        stock_list.add_stock(Stock::new("GOOGL", 1867.88));
+        stock_list.add_stock(Stock::new("MSFT", 244.49));
+        stock_list.add_stock(Stock::new("AMZN", 3242.76));
+        stock_list.add_stock(Stock::new("TSLA", 672.37));
+        Self {
+            should_quit: false,
+            stock_list,
+        }
+    }
+
+    fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        while !self.should_quit {
+            terminal.draw(|frame| frame.render_widget(&mut self, frame.area()))?;
+            if let Event::Key(key) = event::read()? {
+                self.handle_key(key);
+            };
+        }
+        Ok(())
+    }
+
+    fn handle_key(&mut self, key: KeyEvent) {
+        if key.kind != KeyEventKind::Press {
+            return;
+        }
+        match key.code {
+            KeyCode::Char('q') | KeyCode::Esc => self.should_quit = true,
+            KeyCode::Down => self.select_next(),
+            KeyCode::Up => self.select_previous(),
+            _ => {}
+        }
+    }
+
+    fn select_next(&mut self) {
+        self.stock_list.state.select_next();
+    }
+
+    fn select_previous(&mut self) {
+        self.stock_list.state.select_previous();
+    }
+}
+
+impl Widget for &mut App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let [header_area, main_area, _footer_area] = Layout::vertical([
+            Constraint::Length(2),
+            Constraint::Fill(1),
+            Constraint::Length(1),
+        ])
+        .areas(area);
+
+        // let [list_area, item_area] =
+        // Layout::vertical([Constraint::Fill(1), Constraint::Fill(1)]).areas(main_area);
+        let [list_area, item_area] =
+            Layout::horizontal([Constraint::Percentage(50), Constraint::Percentage(50)])
+                .areas(main_area);
+        App::render_header(header_area, buf);
+        self.render_list(list_area, buf);
+        self.render_selected_item(item_area, buf);
+    }
+}
+
+impl App {
+    fn render_header(area: Rect, buf: &mut Buffer) {
+        Paragraph::new("Finance").centered().render(area, buf);
+    }
+
+    fn render_list(&mut self, area: Rect, buf: &mut Buffer) {
+        let block = Block::new().title(Line::raw("Stocks").left_aligned().bold().bg(Color::Cyan));
+
+        let items: Vec<ListItem> = self
+            .stock_list
+            .stocks
+            .iter()
+            .map(|stock| ListItem::new(stock.name.clone()))
+            .collect();
+
+        let list = List::new(items).block(block).highlight_symbol(">");
+
+        StatefulWidget::render(list, area, buf, &mut self.stock_list.state);
+    }
+    fn render_selected_item(&self, area: Rect, buf: &mut Buffer) {
+        // We get the info depending on the item's state.
+        let info = if let Some(i) = self.stock_list.state.selected() {
+            let stock = &self.stock_list.stocks[i];
+            format!("Name: {}\nPrice: ${:.2}", stock.name, stock.price)
+        } else {
+            "Nothing selected...".to_string()
+        };
+
+        // We show the list item's info under the list in this paragraph
+        let block = Block::new()
+            .title(Line::raw("Stock Info").centered())
+            .borders(Borders::TOP)
+            .border_set(symbols::border::EMPTY)
+            .padding(Padding::horizontal(1));
+
+        // We can now render the item info
+        Paragraph::new(info)
+            .block(block)
+            .wrap(Wrap { trim: false })
+            .render(area, buf);
+    }
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let app_result = App::default().run(terminal);
+    ratatui::restore();
+    app_result
 }
