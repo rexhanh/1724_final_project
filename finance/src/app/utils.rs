7361be6060235c9bae93e83bde9dbac441aa44c9
rexhanh <rexhanh123@gmail.com>
2024-11-14 14:55:42 -0500

    use enviroment variable STOCK_API_KEY

--- finance/src/app/utils.rs
+++ finance/src/app/utils.rs
@@ -1,19 +1,22 @@
 use super::model::{Quote, SearchQuote};
-const API_KEY: &str = "uilFVDFWvPNNFgPHkN47tl1vGeusng0H";
+use std::env;
+
 pub fn fetch_search_result(stock: &str) -> Result<Vec<SearchQuote>, reqwest::Error> {
+    let api_key = env::var("STOCK_API_KEY").unwrap();
     let body = reqwest::blocking::Client::new()
         .get("https://financialmodelingprep.com/api/v3/search")
-        .query(&[("query", stock), ("limit", "10"), ("apikey", API_KEY)])
+        .query(&[("query", stock), ("limit", "10"), ("apikey", &api_key)])
         .send()?
         .json::<Vec<SearchQuote>>()?;
     Ok(body)
 }
 
 pub fn fetch_stock(stock: &str) -> Result<Quote, reqwest::Error> {
+    let api_key = env::var("STOCK_API_KEY").unwrap();
     let url = String::from("https://financialmodelingprep.com/api/v3/quote/") + stock;
     let body = reqwest::blocking::Client::new()
         .get(url)
-        .query(&[("apikey", API_KEY)])
+        .query(&[("apikey", api_key)])
         .send()?
         .json::<Vec<Quote>>()?;
     Ok(body[0].clone())
