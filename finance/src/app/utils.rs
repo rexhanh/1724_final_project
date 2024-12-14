4b3180229b15009725b5f1d2e19bc0a3729369ac
Jeff Sheng <85174808+OkayJeff5@users.noreply.github.com>
2024-12-13 21:46:28 -0600

    Update utils.rs

--- finance/src/app/utils.rs
+++ finance/src/app/utils.rs
@@ -1,16 +1,12 @@
 use super::model::{
-    ChartDP, Company, HistoricalPrice, NewsData, Quote, SearchQuote, StockData, StockList, Top,
+    ChartDP, Company, HistoricalPrice, NewsData, Quote, SearchQuote, StockData, StockList, Top, Point,
 };
-use chrono::{Datelike, Duration, NaiveDateTime, Utc,Timelike};
+use chrono::{
+    Datelike, NaiveDateTime, NaiveDate, Utc
+};
 use scraper::{Html, Selector};
 use std::env;
-const API_KEY: &str = "77iRkUzOmkrSxwfuO3Mb8t7gLd6dP9yg";
-// BKo3pwdgStNm3rfLEHuit71sK0mvJBCZ
-// uilFVDFWvPNNFgPHkN47tl1vGeusng0H
-// Bt08M78UNw8jLzvmLk1Bl6s07Gc2rSt6
-// 77iRkUzOmkrSxwfuO3Mb8t7gLd6dP9yg
-// H7iSor1eE79j32YkLqY0czsSfJXhUcDr
-// Qxk93ZPLycDgwKFc0NILS8yzwTsi8a0y
+const API_KEY: &str = "uilFVDFWvPNNFgPHkN47tl1vGeusng0H";
 
 // If the environment variable is set, use it. Otherwise, use the default API key.
 fn get_api_key() -> String {
@@ -74,6 +70,26 @@ pub fn fetch_sma(stock: &str, period: &str) -> Result<Vec<ChartDP>, reqwest::Err
     Ok(body)
 }
 
+pub async fn fetch_sma_async(stock: &str, period: &str) -> Result<Vec<ChartDP>, reqwest::Error> {
+    let api_key = get_api_key();
+    let url = format!("https://financialmodelingprep.com/api/v3/technical_indicator/1day/{}", stock);
+
+    let client = reqwest::Client::new();
+    let response = client
+        .get(&url)
+        .query(&[
+            ("apikey", &api_key),
+            ("period", &period.to_owned()),
+            ("type", &"sma".to_owned()),
+        ])
+        .send()
+        .await?;
+
+    let data = response.json::<Vec<ChartDP>>().await?;
+    Ok(data)
+}
+
+// for tui plot
 pub fn parse_chart_point(point: &ChartDP, year: i32) -> Option<(f64, f64)> {
     // Parse the date string to NaiveDateTime
     let datetime = NaiveDateTime::parse_from_str(&point.date, "%Y-%m-%d %H:%M:%S").ok()?;
@@ -86,6 +102,30 @@ pub fn parse_chart_point(point: &ChartDP, year: i32) -> Option<(f64, f64)> {
     } else {
         None // Return None if the year does not match
     }
+}
+
+// for web plot: filter and reversse
+pub fn filter(data: Vec<ChartDP>) -> Vec<(String, f64)> {
+    let current_year = Utc::now().year();
+
+    let mut filtered_data: Vec<_> = data
+        .into_iter()
+        .filter_map(|item| {
+            // Parse the date and remove the time
+            if let Ok(parsed_date) = NaiveDate::parse_from_str(&item.date[..10], "%Y-%m-%d") {
+                // Only include data for the current year
+                if parsed_date.year() == current_year {
+                    return Some((parsed_date.format("%Y-%m-%d").to_string(), item.value));
+                }
+            }
+            None
+        })
+        .collect();
+
+    // Reverse the order to go from least to most recent
+    filtered_data.reverse();
+
+    filtered_data
 }
 
 // get 1 ds<(f64, f64)>, find x-bound, y-bound
@@ -153,61 +193,24 @@ pub fn get_top_gainers() -> Result<Vec<Top>, reqwest::Error> {
     Ok(body)
 }
 
-pub fn fetch_intraday_data(symbol: &str) -> Result<Vec<HistoricalPrice>, reqwest::Error> {
-    // Get current time in Eastern Time
-    let now_est = chrono::Utc::now().with_timezone(&chrono_tz::America::New_York);
-
-    // Get current hour and minute in EST
-    let current_hour = now_est.hour();
-    let current_minute = now_est.minute();
-
-    // Determine the date to use
-    let mut current_date = if current_hour < 9 || (current_hour == 9 && current_minute < 30) {
-        // If before 09:30 AM, use the previous day's date
-        now_est.naive_local().date() - Duration::days(1)
-    } else {
-        // Otherwise, use today's date
-        now_est.naive_local().date()
-    };
-
-    loop {
-        let current_date_str = current_date.format("%Y-%m-%d").to_string();
-
-        // Fetch intraday data for the determined date
-        let url = format!(
-            "https://financialmodelingprep.com/api/v3/historical-chart/5min/{}",
-            symbol
-        );
-        let response = reqwest::blocking::Client::new()
-            .get(&url)
-            .query(&[
-                ("apikey", API_KEY),
-                ("from", &current_date_str),
-                ("to", &current_date_str),
-            ])
-            .send();
-
-        if let Ok(res) = response {
-            if res.status().is_success() {
-                let data = res.json::<Vec<HistoricalPrice>>()?;
-                if !data.is_empty() {
-                    // If data exists, return it
-                    return Ok(data);
-                }
-            }
-        }
-
-        // If no data, decrement the date and continue
-        current_date = current_date - Duration::days(1);
-    }
+pub fn fetch_intraday_data(symbol: &str, current_date:&str) -> Result<Vec<HistoricalPrice>, reqwest::Error> {
+    let url = format!(
+        "https://financialmodelingprep.com/api/v3/historical-chart/5min/{}",
+        symbol
+    );
+    let response = reqwest::blocking::Client::new()
+        .get(&url)
+        .query(&[
+            ("apikey", API_KEY),
+            ("from", &current_date),
+            ("to", &current_date),
+        ])
+        .send()?
+        .json::<Vec<HistoricalPrice>>()?;
+    Ok(response)
 }
 
-
-pub fn fetch_historical_data(
-    symbol: &str,
-    from: &str,
-    to: &str,
-) -> Result<StockData, reqwest::Error> {
+pub fn fetch_full_historical_data(symbol: &str) -> Result<StockData, reqwest::Error> {
     let api_key = get_api_key();
     let url = format!(
         "https://financialmodelingprep.com/api/v3/historical-price-full/{}",
@@ -215,22 +218,10 @@ pub fn fetch_historical_data(
     );
     let body = reqwest::blocking::Client::new()
         .get(&url)
-        .query(&[("from", from), ("to", to), ("apikey", &api_key)])
+        .query(&[("apikey", &api_key)])
         .send()?
         .json::<StockData>()?;
     Ok(body)
-}
-
-pub fn get_month_data_range() -> (String, String) {
-    let today = Utc::now().naive_utc().date();
-    let one_month_ago = today - Duration::days(30);
-    (one_month_ago.to_string(), today.to_string())
-}
-
-pub fn get_year_data_range() -> (String, String) {
-    let today = Utc::now().naive_utc().date();
-    let one_year_ago = today - Duration::days(365);
-    (one_year_ago.to_string(), today.to_string())
 }
 
 pub fn get_news() -> Result<NewsData, reqwest::Error> {
@@ -251,4 +242,53 @@ pub fn parse_news(html: Html) -> Vec<String> {
         paragraphs.push(node.text().collect());
     }
     paragraphs
+}
+
+// detect golden cross and death cross
+pub fn detect_intersections(
+    sma1: &Vec<(String, f64)>,
+    sma2: &Vec<(String, f64)>,
+) -> (Vec<(String, f64)>, Vec<(String, f64)>) {
+    let mut golden_crosses = Vec::new();
+    let mut death_crosses = Vec::new();
+    let dates: Vec<_> = sma1.iter().map(|(date, _)| date.clone()).collect();
+
+    for i in 0..sma1.len() - 1 {
+        let p1 = Point { x: i as f64, y: sma1[i].1 };
+        let q1 = Point { x: (i + 1) as f64, y: sma1[i + 1].1 };
+        let p2 = Point { x: i as f64, y: sma2[i].1 };
+        let q2 = Point { x: (i + 1) as f64, y: sma2[i + 1].1 };
+
+        let denom = (q1.y - p1.y) - (q2.y - p2.y);
+        if denom.abs() > 1e-9 {
+            let t = (p2.y - p1.y) / denom;
+            if t >= 0.0 && t <= 1.0 {
+                let x_intersection = p1.x + t * (q1.x - p1.x);
+                let y_intersection = p1.y + t * (q1.y - p1.y);
+
+                // Determine the type of crossover
+                if sma1[i].1 < sma2[i].1 && sma1[i + 1].1 > sma2[i + 1].1 {
+                    // Golden Cross
+                    let index = x_intersection.floor() as usize;
+                    let date = if index + 1 < dates.len() {
+                        dates[index].clone()
+                    } else {
+                        dates[index].clone()
+                    };
+                    golden_crosses.push((date, y_intersection));
+                } else if sma1[i].1 > sma2[i].1 && sma1[i + 1].1 < sma2[i + 1].1 {
+                    // Death Cross
+                    let index = x_intersection.floor() as usize;
+                    let date = if index + 1 < dates.len() {
+                        dates[index].clone()
+                    } else {
+                        dates[index].clone()
+                    };
+                    death_crosses.push((date, y_intersection));
+                }
+            }
+        }
+    }
+
+    (golden_crosses, death_crosses)
 }
